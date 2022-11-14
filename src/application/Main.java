16ff5f7526277ea283a3987c803f7d2420db246f
javiercubas <javiercubas2003@gmail.com>
2022-11-14 16:29:02 +0100

    update

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,9 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
@@ -10,10 +12,14 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
+			primaryStage.setTitle("Ocurex");
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/application/SeleccionUsuario.fxml"));
+			ControladorOcurex controlador = new ControladorOcurex();
+			loader.setController(controlador);
+			Parent root = loader.load();
+			
+			
+			primaryStage.setScene(new Scene(root));
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
